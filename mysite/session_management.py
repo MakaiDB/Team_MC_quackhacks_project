aee33f1c9aabd594a9e638d03b2d7fb0af29efeb
MakaiDB <makai.deberry@gmail.com>
2025-11-15 23:18:21 -0800

    debugging models.py imports

--- mysite/session_management.py
+++ mysite/session_management.py
@@ -39,7 +39,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 django.setup()
 
-from mysite.models import Session
+from models import Session
 import random
 
 
